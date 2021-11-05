63a1b55a3563a40169b4fffb6059510c2aa279e0
rahimhamid <78023684+rahimhamid@users.noreply.github.com>
2021-11-05 02:04:50 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ moneyType = 'bank'
 -- Add locations here
 -- Add jobs specific to the garage.
 bennyGarages = {
-    [1] = {coords = vector4(-211.55, -1324.55, 30.90, 319.73135375977), useJob = false, job = {"mechanic", "police"}},
+    [1] = {coords = vector4(-33.4, -1055.85, 28.4, 38.23), useJob = false, job = {"mechanic", "police"}},
     [2] = {coords = vector4(109.89, 6627.07, 31.78, 221.7938), useJob = true, job = {"mechanic"}}
 }
 
